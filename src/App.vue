04772549f00f6f1a4bfc16c81afe60cedfff0856
FaisalFeroze115 <51878386+FaisalFeroze115@users.noreply.github.com>
2021-11-03 18:36:56 +0600

    playing with vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>TO DO APP</h1>
+  <Todo/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todo
   }
 }
 </script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="inputTodo"/>
+      <button type="submit">ADD</button>
+    </form>
+
+    <div>
+      <ul>
+        <li v-for="(todo,index) in todolists" :key="index">
+          {{todo}} <span v-on:click="remove(index)">x</span>
+        </li>
+      </ul>
+    </div>
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  data(){
+    return{
+      inputTodo: '',
+      todolists: []
+    }
+  },
+  methods:{
+    addTodo(){
+      this.todolists.push(this.inputTodo);
+      this.inputTodo = "";
+    },
+    remove(id){
+      const newTodo = this.todolists.filter((el,index) => index !== id);
+      this.todolists = newTodo;
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
